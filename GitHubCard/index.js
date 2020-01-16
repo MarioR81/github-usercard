c3962deaf57e22e27fcf0b07832bb14ada81445d
apple <apple@apples-MacBook-Pro-2.local>
2020-01-16 12:18:59 -0800

    setup project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,7 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+// console.log(axios.get('http://api.github.com/users/marior81'));
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -25,6 +25,11 @@
 */
 
 const followersArray = [];
+
+function createCards(object){
+  
+
+};
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
